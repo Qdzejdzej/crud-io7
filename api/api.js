5aa1ea6c1d387fd9becdbca3394ab68629ee474e
Mateo <mateok@onet.eu>
2019-01-10 19:35:25 +0100

    add api configuration

---
+++ api/api.js
@@ -0,0 +1,22 @@
+const express     = require('express'),
+      bodyParser  = require('body-parser'),
+      path        = require('path'),
+      cors        = require('cors'),
+      mongoose    = require('mongoose');
+
+const DB = 'mongodb://localhost:27017/crud';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function(){
+ console.log('Listening on port ' + port);
+});
